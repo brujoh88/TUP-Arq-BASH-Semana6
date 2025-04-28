c9e227d2ace436fd4cbe8fa26e4b1fcc05bb9e8d
Gus - MVC <gustavotiseira@gmail>
2025-04-28 09:13:06 -0300

    ejercicio 1.2

---
+++ ejercicio1.2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Autor: [Tiseira Gustavo]
+# Fecha: [28/10/2025]
+
+clear
+read -p "Por favor, ingrese el nombre de usuario al que desea enviar un mensaje:" nombre_usuario
+read -p "Por favor, ingrese el mensaje que desea enviar:" mensaje
+echo "$mensaje" | write $nombre_usuario
