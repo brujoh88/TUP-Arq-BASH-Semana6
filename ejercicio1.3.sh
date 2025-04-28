6240ef6958cc992ba75d7884f0a65ca282d7ca0a
Gus - MVC <gustavotiseira@gmail>
2025-04-28 09:21:19 -0300

    ejercicio 1.3

---
+++ ejercicio1.3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Autor: [Tiseira Gustavo]
+# Fecha: [28/10/2025]
+
+clear
+echo "2."
+echo "El parametro \$1 es: $1"
+echo "El parametro \$2 es: $2"
+echo "El parametro \$3 es: $3"
+echo "El parametro \$4 es: $4"
+echo "El parametro \$5 es: $5"
+echo "El parametro \$6 es: $6"
+echo "El parametro \$7 es: $7"
+echo "El parametro \$8 es: $8"
+echo "El parametro \$9 es: $9"
+echo "3."
+echo "El total de parametros es: $#"
+echo "4."
+echo "Los parametros concatenados son: $*"
+echo "5."
+echo "El nombre del archivo ejecutado es: $0"
