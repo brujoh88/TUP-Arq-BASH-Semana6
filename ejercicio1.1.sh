392b292c152a3865dd6750f9c5f422b9f6ec0e46
Gus - MVC <gustavotiseira@gmail>
2025-04-28 08:36:09 -0300

    init

---
+++ ejercicio1.1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Autor: [Tiseira Gustavo]
+# Fecha: [25/10/2025]
+clear
+echo "Nombre del usuario: $LOGNAME [LOGNAME]"
+echo "El directorio home del usuario es: $HOME [HOME]"
+echo "La shell que se está utilizando es: $SHELL [SHELL]"
+echo "El directorio actual es: $PWD [PWD]"
+echo "El usuario que ha iniciado sesión es: $USER [USER]"
+echo "La terminal asociado a la sesión SSH, si está disponible, es: $SSH_TTY [SSH_TTY]"
